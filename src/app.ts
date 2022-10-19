85ebc3636c882a6ea48369585bbb7e38ad8ddc94
Oscar Solorzano <oscar.solorzano98@gmail.com>
2022-10-19 17:58:10 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import morgan from 'morgan'
 import { globalErrorHandler } from './controllers/error.controller'
 import routers from './routes'
-
+import cors from 'cors'
 // Init our Express app
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cors())
 app.use('/api/v1', routers)
 app.use(globalErrorHandler)
 
